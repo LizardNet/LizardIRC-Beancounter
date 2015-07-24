de9eee5176455abccb3bfe4336956b2ae3578d2c
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2015-07-24 15:35:36 -0700

    Add AccessControl class, enable ACL for ?quit cmd
    Adds the AccessControl class, which does hostmask-based permissions checking
    for running commands, and added AccessControl protection for the ?quit command.
    
    Currently, the AccessControl list (or should I say HashMap?) is hard-coded to
    grant all permissions to *!*@lizardirc/staff/* and nothing else.  This should
    eventually be changed to pull in configuration a la java.util.Properties.
    
    Change-Id: I5f13aaf5a7a3e1182b877c1a586b6b8acbe5650b

---
+++ src/main/java/org/lizardirc/beancounter/AccessControl.java
@@ -0,0 +1,78 @@
+/**
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2015 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+
+package org.lizardirc.beancounter;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+public class AccessControl {
+    private Map<String, String> accessList;
+
+    public AccessControl(Map<String, String> accessList) {
+        if (accessList.isEmpty()) {
+            System.err.println("Warning: AccessControl insantiated with empty accessList.");
+            System.err.println("Assuming everyone is authorized to access everything!");
+            accessList.put("^.*!.*@.*$", "*");
+        }
+        this.accessList = accessList;
+    }
+
+    public Map<String, String> getAccessList() {
+        return accessList;
+    }
+
+    public void setAccessList(Map<String, String> accessList) {
+        this.accessList = accessList;
+    }
+
+    public boolean hasPriv(GenericMessageEvent<?> event, String permission) {
+        // Apparently, PircBotX has a different definition of "hostmask" than the rest of the IRC world....
+        String userHostmask = event.getUser().getNick() + "!" + event.getUser().getLogin() + "@" + event.getUser().getHostmask();
+
+        for (Entry<String, String> entry : accessList.entrySet()) {
+            String aclHostmask = entry.getKey();
+            String aclPermission = entry.getValue();
+
+            if (userHostmask.matches(aclHostmask)) {
+                if (permission == aclPermission || aclPermission == "*") {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
