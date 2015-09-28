b3df13afb145e70e5e473ec5dbf2d6315607d8b1
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2015-09-27 23:57:58 -0700

    Catch and kill very long-running regexes in SedListener
    Change-Id: I4809c3b81b59380b34dbcd2f516d9452ba606dce

--- src/main/java/org/lizardirc/beancounter/SedListener.java
+++ src/main/java/org/lizardirc/beancounter/SedListener.java
@@ -33,6 +33,8 @@
 package org.lizardirc.beancounter;
 
 import java.util.Queue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -41,10 +43,15 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.EvictingQueue;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import com.google.common.util.concurrent.TimeLimiter;
+import com.google.common.util.concurrent.UncheckedTimeoutException;
 import org.pircbotx.PircBotX;
 import org.pircbotx.User;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
+
+import org.lizardirc.beancounter.utils.InterruptibleCharSequence;
 
 public class SedListener<T extends PircBotX> extends ListenerAdapter<T> {
     // There are several substring types that we match.
@@ -68,7 +75,10 @@ public class SedListener<T extends PircBotX> extends ListenerAdapter<T> {
 
     private static final Pattern PATTERN_OPTIONS = Pattern.compile("[gi]*");
 
-    private final LoadingCache<User, Queue<String>> windows;
+    private final LoadingCache<User, Queue<InterruptibleCharSequence>> windows;
+    private final TimeLimiter timeLimiter = new SimpleTimeLimiter();
+
+    private MessageEvent<T> event;
 
     public SedListener(int windowSize) {
         windows = CacheBuilder.newBuilder()
@@ -76,64 +86,82 @@ public class SedListener<T extends PircBotX> extends ListenerAdapter<T> {
     }
 
     @Override
-    public void onMessage(MessageEvent<T> event) {
-        String message = event.getMessage();
-        Matcher m = PATTERN_SED.matcher(message);
-        if (m.matches()) {
-            String target = m.group(1);
-            String regex = m.group(3);
-            String replacement = m.group(4);
-            String options = m.group(5);
+    public void onMessage(MessageEvent<T> event) throws Exception {
+        this.event = event;
+        try {
+            timeLimiter.callWithTimeout(new SedListenerCallable(), 5, TimeUnit.SECONDS, false);
+        } catch (UncheckedTimeoutException e) {
+            event.respond("Timeout while processing replacement.");
+            System.err.println("WARNING: " + event.getUser().getNick() + " caused regex timeout with regex " + event.getMessage() + ".");
+        }
+    }
 
-            User corrector = event.getUser();
-            User speaker = event.getChannel().getUsers().stream()
-                .filter(u -> u.getNick().equals(target))
-                .findFirst()
-                .orElse(corrector);
+    private class SedListenerCallable implements Callable<Void> {
+        @Override
+        public Void call() throws Exception {
+            doReplacement();
+            return null;
+        }
 
-            if (!PATTERN_OPTIONS.matcher(options).matches()) {
-                event.respond("Invalid options '" + options + "'");
-                return;
+        private void doReplacement() {
+            String message = event.getMessage();
+            Matcher m = PATTERN_SED.matcher(new InterruptibleCharSequence(message));
+            if (m.matches()) {
+                String target = m.group(1);
+                String regex = m.group(3);
+                String replacement = m.group(4);
+                String options = m.group(5);
+
+                User corrector = event.getUser();
+                User speaker = event.getChannel().getUsers().stream()
+                    .filter(u -> u.getNick().equals(target))
+                    .findFirst()
+                    .orElse(corrector);
+
+                if (!PATTERN_OPTIONS.matcher(options).matches()) {
+                    event.respond("Invalid options '" + options + "'");
+                    return;
+                }
+
+                int flags = 0;
+                if (options.contains("i")) {
+                    flags = Pattern.CASE_INSENSITIVE;
+                }
+
+                Pattern p;
+                try {
+                    p = Pattern.compile(regex, flags);
+                } catch (PatternSyntaxException e) {
+                    event.respond("Invalid regex '" + regex + "': " + e.getMessage());
+                    return;
+                }
+
+                Queue<InterruptibleCharSequence> window = windows.getUnchecked(speaker);
+
+                window.stream()
+                    .map(p::matcher)
+                    .filter(Matcher::find)
+                    .reduce((x, y) -> y) // findLast()
+                    .map(matcher -> {
+                        if (options.contains("g")) {
+                            return matcher.replaceAll(replacement);
+                        } else {
+                            return matcher.replaceFirst(replacement);
+                        }
+                    })
+                    .ifPresent(s -> {
+                        window.add(new InterruptibleCharSequence(s));
+
+                        StringBuilder sb = new StringBuilder(corrector.getNick());
+                        if (!corrector.equals(speaker)) {
+                            sb.append(" thinks ").append(speaker.getNick());
+                        }
+                        sb.append(" meant to say: ").append(s);
+                        event.getChannel().send().message(sb.toString());
+                    });
+            } else {
+                windows.getUnchecked(event.getUser()).add(new InterruptibleCharSequence(message));
             }
-
-            int flags = 0;
-            if (options.contains("i")) {
-                flags = Pattern.CASE_INSENSITIVE;
-            }
-
-            Pattern p;
-            try {
-                p = Pattern.compile(regex, flags);
-            } catch (PatternSyntaxException e) {
-                event.respond("Invalid regex '" + regex + "': " + e.getMessage());
-                return;
-            }
-
-            Queue<String> window = windows.getUnchecked(speaker);
-
-            window.stream()
-                .map(p::matcher)
-                .filter(Matcher::find)
-                .reduce((x, y) -> y) // findLast()
-                .map(matcher -> {
-                    if (options.contains("g")) {
-                        return matcher.replaceAll(replacement);
-                    } else {
-                        return matcher.replaceFirst(replacement);
-                    }
-                })
-                .ifPresent(s -> {
-                    window.add(s);
-
-                    StringBuilder sb = new StringBuilder(corrector.getNick());
-                    if (!corrector.equals(speaker)) {
-                        sb.append(" thinks ").append(speaker.getNick());
-                    }
-                    sb.append(" meant to say: ").append(s);
-                    event.getChannel().send().message(sb.toString());
-                });
-        } else {
-            windows.getUnchecked(event.getUser()).add(message);
         }
     }
 }
