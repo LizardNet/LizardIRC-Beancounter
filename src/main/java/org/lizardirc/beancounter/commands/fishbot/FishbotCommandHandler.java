77b23c8bdb19cc92649f0dfe09362cda43dc75f4
Simon Walker <github@stwalkerster.co.uk>
2016-08-11 23:43:44 +0100

    Add fishb0t to Beancounter
    Change-Id: Iddf202cc4a8f813e334227f2da752d8710ce6c3c

---
+++ src/main/java/org/lizardirc/beancounter/commands/fishbot/FishbotCommandHandler.java
@@ -0,0 +1,143 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2016 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+package org.lizardirc.beancounter.commands.fishbot;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.types.GenericChannelEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import org.lizardirc.beancounter.hooks.CommandHandler;
+
+class FishbotCommandHandler<T extends PircBotX> implements CommandHandler<T> {
+    private static final String CMD_FISHBOT = "fishbot";
+
+    private static final Set<String> COMMANDS = ImmutableSet.of(CMD_FISHBOT);
+
+    private static final String SUBCMD_ON = "on";
+    private static final String SUBCMD_OFF = "off";
+    private static final String SWITCH_FORCE = "--force";
+    private static final Set<String> SUBCOMMANDS = ImmutableSet.of(SUBCMD_ON, SUBCMD_OFF, SWITCH_FORCE);
+
+    private static final String PERM_GLOBAL_FISHBOT = "globalFishbot";
+
+    private final FishbotListener<T> parentListener;
+
+    FishbotCommandHandler(FishbotListener<T> parentListener) {
+        this.parentListener = parentListener;
+    }
+
+    @Override
+    public Set<String> getSubCommands(GenericMessageEvent<T> event, List<String> commands) {
+        if (commands.isEmpty()) {
+            return COMMANDS;
+        } else {
+            return SUBCOMMANDS;
+        }
+    }
+
+    @Override
+    public void handleCommand(GenericMessageEvent<T> event, List<String> commands, String remainder) {
+        if (commands.isEmpty() || !CMD_FISHBOT.equals(commands.get(0))) {
+            return;
+        }
+
+        if (!(event instanceof GenericChannelEvent)) {
+            event.respond("This command may only be run in a channel.");
+            return;
+        }
+
+        if (commands.size() == 1) {
+            // ?fishbot  - report current status
+            boolean enabled = parentListener.getStatus(((GenericChannelEvent) event).getChannel().getName());
+            String status = enabled ? "enabled" : "disabled";
+            event.respond("fishb0t is currently " + status + " in this channel.");
+        }
+
+        if (commands.size() >= 2) {
+            boolean forceSet = false;
+            boolean onSet = false;
+            boolean offSet = false;
+
+            for (String sc : commands) {
+                switch (sc) {
+                    case SWITCH_FORCE:
+                        forceSet = true;
+                        break;
+                    case SUBCMD_OFF:
+                        offSet = true;
+                        break;
+                    case SUBCMD_ON:
+                        onSet = true;
+                        break;
+                }
+            }
+
+            if (onSet && offSet || !onSet && !offSet) {
+                event.respond("Please choose between turning fishb0t on, or turning fishb0t off.");
+                return;
+            }
+
+            if (forceSet) {
+                if (!parentListener.getAccessControl().hasPermission(event, PERM_GLOBAL_FISHBOT)) {
+                    event.respond("No u!  You do not have permission to use the " + SWITCH_FORCE + " switch.");
+                    return;
+                }
+            } else {
+                // ?fishbot on|off  - change status
+                if (!((GenericChannelEvent) event).getChannel().isOp(event.getUser())) {
+                    event.respond("You must be a channel operator to enable or disable fishb0t.");
+
+                    if (parentListener.getAccessControl().hasPermission(event, PERM_GLOBAL_FISHBOT)) {
+                        event.respond("Alternatively, since you have the requisite global bot permissions, you can use the \"" +
+                                SWITCH_FORCE + "\" switch to override this.");
+                    }
+
+                    return;
+                }
+            }
+
+            if (onSet) {
+                parentListener.enable(((GenericChannelEvent) event).getChannel().getName());
+                event.respond("Done");
+            }
+
+            if (offSet) {
+                parentListener.disable(((GenericChannelEvent) event).getChannel().getName());
+                event.respond("Done");
+            }
+        }
+    }
+}
---
+++ src/main/java/org/lizardirc/beancounter/commands/fishbot/FishbotResponseRepository.java
@@ -0,0 +1,78 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2016 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+package org.lizardirc.beancounter.commands.fishbot;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import org.lizardirc.beancounter.Beancounter;
+
+public class FishbotResponseRepository {
+    private List<FishbotResponse> messages;
+    private List<FishbotResponse> actions;
+
+    private FishbotResponseRepository(List<FishbotResponse> messages, List<FishbotResponse> actions) {
+        this.messages = messages;
+        this.actions = actions;
+    }
+
+    public static FishbotResponseRepository initialise() {
+        try (InputStream fishbotFile = Beancounter.class.getResourceAsStream("/fishbot.json")) {
+            Gson fishbotDeserialiser = new Gson();
+            InputStreamReader repositoryReader = new InputStreamReader(fishbotFile);
+
+            return fishbotDeserialiser.fromJson(repositoryReader, FishbotResponseRepository.class);
+        } catch (IOException | NullPointerException e) {
+            System.err.println("Caught IOException or NullPointerException trying to read in fishb0t m00s: " + e.getMessage());
+            e.printStackTrace();
+            return new FishbotResponseRepository(new ArrayList<>(), new ArrayList<>());
+        }
+    }
+
+    List<FishbotResponse> getMessages() {
+        return messages;
+    }
+
+    List<FishbotResponse> getActions() {
+        return actions;
+    }
+
+    synchronized void recompile(String nickname) {
+        this.messages.forEach(r -> r.compile(nickname));
+        this.actions.forEach(r -> r.compile(nickname));
+    }
+}
