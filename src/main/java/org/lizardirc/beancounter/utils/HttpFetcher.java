998b5e04d52ab30ff1e89a25cbc79f757a7f5046
Simon Walker <github@stwalkerster.co.uk>
2016-08-20 14:44:02 +0100

    Implement Wikipedia article lookup command
    This currently always defaults to the English Wikipedia, future
    changes could extend this to support any MediaWiki command via
    configuration - and the support is in place to handle any MediaWiki
    instance with an enabled and accessible API.
    
    Testing has been done with lookups on both articles, redirects, and
    non-existent pages.
    
    Fixes: #16
    Change-Id: If785e44c6a7c606ccba752fa09f28b7f11b0cc67

---
+++ src/main/java/org/lizardirc/beancounter/utils/HttpFetcher.java
@@ -0,0 +1,58 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2016 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+
+package org.lizardirc.beancounter.utils;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public final class HttpFetcher {
+
+    // TODO: externalise this to bot configuration
+    public static final String USERAGENT = "Beancounter/0.0 (development version, /u/stwalkerster";
+
+    public static String getPageContent(URI url) throws IOException {
+        HttpGet request = new HttpGet(url);
+        request.addHeader("User-Agent", USERAGENT);
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        HttpClient httpClient = httpClientBuilder.build();
+        HttpResponse response = httpClient.execute(request);
+
+        return EntityUtils.toString(response.getEntity());
+    }
+}
