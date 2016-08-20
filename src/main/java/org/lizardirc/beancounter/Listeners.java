44b12db3b380559924783466c5e1f38b56ca5805
Simon Walker <github@stwalkerster.co.uk>
2016-08-20 23:29:08 +0100

    [WIP] Automatic link expansion
    Change-Id: I86c18e4332c243790448145a99bc6e1e567e76f2

--- src/main/java/org/lizardirc/beancounter/Listeners.java
+++ src/main/java/org/lizardirc/beancounter/Listeners.java
@@ -66,6 +66,7 @@ import org.lizardirc.beancounter.commands.sed.SedListener;
 import org.lizardirc.beancounter.commands.seen.UserLastSeenListener;
 import org.lizardirc.beancounter.commands.shakespeare.ShakespeareHandler;
 import org.lizardirc.beancounter.commands.slap.SlapHandler;
+import org.lizardirc.beancounter.commands.url.UrlListener;
 import org.lizardirc.beancounter.commands.weather.WeatherHandler;
 import org.lizardirc.beancounter.commands.wikipedia.WikipediaHandler;
 import org.lizardirc.beancounter.commands.youtube.YouTubeHandler;
@@ -136,15 +137,16 @@ public class Listeners<T extends PircBotX> implements CommandHandler<T> {
 
         boolean enableWeatherHandler = Boolean.parseBoolean(properties.getProperty("weather.enable", "false"));
 
+        YouTubeService youTubeService = new YouTubeService(pm.getNamespace("youtube"));
+
         acl = new BreadBasedAccessControl<>(ownerHostmask, pm.getNamespace("breadBasedAccessControl"));
         UserLastSeenListener<T> userLastSeenListener = new UserLastSeenListener<>(pm.getNamespace("userLastSeenConfig"), acl);
         InviteAcceptor<T> inviteAcceptor = new InviteAcceptor<>(pm.getNamespace("inviteAcceptor"), acl);
         ReminderListener<T> reminderListener = new ReminderListener<>(pm.getNamespace("reminderHandler"), acl, scheduledExecutorService);
         EarthquakeListener<T> earthquakeListener = new EarthquakeListener<>(pm.getNamespace("earthquakeListener"), acl, scheduledExecutorService);
         EntryMessageListener<T> entryMessageListener = new EntryMessageListener<>(pm.getNamespace("entryMessage"), acl);
+        UrlListener<T> urlListener = new UrlListener<>(youTubeService);
         FishbotListener<T> fishbotHandler = new FishbotListener<>(FishbotResponseRepository.initialise(), pm.getNamespace("fishbot"), acl);
-
-        YouTubeService youTubeService = new YouTubeService(pm.getNamespace("youtube"));
 
         List<CommandHandler<T>> handlers = new ArrayList<>();
         handlers.add(new AdminHandler<>(acl));
@@ -184,6 +186,7 @@ public class Listeners<T extends PircBotX> implements CommandHandler<T> {
         ownListeners.add(reminderListener);
         ownListeners.add(earthquakeListener);
         ownListeners.add(entryMessageListener);
+        ownListeners.add(urlListener);
         ownListeners.add(fishbotHandler);
 
         ownListeners.forEach(listenerManager::addListener);
