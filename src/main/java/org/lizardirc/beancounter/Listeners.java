6ceaf95a5f0b883d5ddcbcb03f21a7b26dcf0583
Simon Walker <github@stwalkerster.co.uk>
2016-08-20 15:03:18 +0100

    [WIP] YouTube command implementation
    Change-Id: I78faffb0584a10d0463442664a750345559e62e0

--- src/main/java/org/lizardirc/beancounter/Listeners.java
+++ src/main/java/org/lizardirc/beancounter/Listeners.java
@@ -67,6 +67,8 @@ import org.lizardirc.beancounter.commands.seen.UserLastSeenListener;
 import org.lizardirc.beancounter.commands.slap.SlapHandler;
 import org.lizardirc.beancounter.commands.weather.WeatherHandler;
 import org.lizardirc.beancounter.commands.wikipedia.WikipediaHandler;
+import org.lizardirc.beancounter.commands.youtube.YouTubeHandler;
+import org.lizardirc.beancounter.commands.youtube.YouTubeService;
 import org.lizardirc.beancounter.hooks.Chainable;
 import org.lizardirc.beancounter.hooks.CommandHandler;
 import org.lizardirc.beancounter.hooks.CommandListener;
@@ -141,6 +143,8 @@ public class Listeners<T extends PircBotX> implements CommandHandler<T> {
         EntryMessageListener<T> entryMessageListener = new EntryMessageListener<>(pm.getNamespace("entryMessage"), acl);
         FishbotListener<T> fishbotHandler = new FishbotListener<>(FishbotResponseRepository.initialise(), pm.getNamespace("fishbot"), acl);
 
+        YouTubeService youTubeService = new YouTubeService(pm.getNamespace("youtube"));
+
         List<CommandHandler<T>> handlers = new ArrayList<>();
         handlers.add(new AdminHandler<>(acl));
         handlers.add(new DiceHandler<>());
@@ -150,6 +154,7 @@ public class Listeners<T extends PircBotX> implements CommandHandler<T> {
         handlers.add(new PerChannelCommand<>(RouletteHandler::new));
         handlers.add(new WikipediaHandler<>());
         handlers.add(new RedditHandler<>());
+        handlers.add(new YouTubeHandler<>(acl, youTubeService));
         handlers.add(acl.getHandler());
         handlers.add(userLastSeenListener.getCommandHandler());
         if (enableWeatherHandler) {
