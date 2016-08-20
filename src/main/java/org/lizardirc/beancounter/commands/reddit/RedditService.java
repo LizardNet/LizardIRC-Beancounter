962ba3fb183f2b938e43130160140a844bc4d718
Simon Walker <github@stwalkerster.co.uk>
2016-08-20 14:44:23 +0100

    Implement Reddit lookup command
    Change-Id: I645ceb4a0b4e963aac1eec0642fba8c72ea6f164

---
+++ src/main/java/org/lizardirc/beancounter/commands/reddit/RedditService.java
@@ -0,0 +1,79 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2016 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+package org.lizardirc.beancounter.commands.reddit;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.client.utils.URIBuilder;
+
+import org.lizardirc.beancounter.utils.Miscellaneous;
+
+public class RedditService {
+    public RedditUser getRedditUser(String username) throws URISyntaxException, IOException {
+        URI url = new URIBuilder()
+                .setScheme("https")
+                .setHost("www.reddit.com")
+                .setPath("/user/" + username + "/about.json")
+                .build();
+
+        String pageContent = Miscellaneous.getHttpData(url);
+
+        JsonObject element = new JsonParser().parse(pageContent).getAsJsonObject();
+        Gson gson = new Gson();
+        return gson.fromJson(element.get("data"), RedditUser.class);
+    }
+
+    public RedditPost getRedditPost(String postId) throws URISyntaxException, IOException {
+        URI url = new URIBuilder()
+                .setScheme("https")
+                .setHost("www.reddit.com")
+                .setPath("/comments/" + postId + ".json")
+                .addParameter("limit", "1")
+                .build();
+
+        String pageContent = Miscellaneous.getHttpData(url);
+
+        JsonArray document = new JsonParser().parse(pageContent).getAsJsonArray();
+        JsonObject element = document.get(0).getAsJsonObject();
+        Gson gson = new Gson();
+        JsonArray children = element.get("data").getAsJsonObject().getAsJsonArray("children");
+        JsonElement data = children.get(0).getAsJsonObject().getAsJsonObject("data");
+        return gson.fromJson(data, RedditPost.class);
+    }
+}
