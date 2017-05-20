e099c6b0ddce9d62f433dab00dae01020a254949
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2017-05-20 02:45:42 -0700

    Add ?airport command
    The ?airport command allows a user to lookup any civil airport (and some
    military airports) in the world by name substring match, by IATA code, or by
    ICAO/GPS code.
    
    Change-Id: I98486739076ae3fc65dab5c8ca4372f88df8497d

---
+++ src/main/java/org/lizardirc/beancounter/commands/airport/Airport.java
@@ -0,0 +1,12 @@
+package org.lizardirc.beancounter.commands.airport;
+
+class Airport {
+    public String iataCode;
+    public String icaoCode;
+    public String name;
+    public String type;
+    public String[] runways;
+    public String municipality;
+    public String regionAndCountry;
+    public String continent;
+}
