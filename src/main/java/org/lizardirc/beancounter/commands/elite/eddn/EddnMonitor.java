2463a7e9e4c89b2cc6ca87809f281528ac16515f
Andrew "FastLizard4" Adams <FastLizard4-Git-Commits@fastlizard4.org>
2017-06-13 01:17:15 -0700

    Add Elite: Dangerous commands
    Adds the ?cmdr command to the bot to look up the information of an Elite:
    Dangerous player from the Elite Dangerous Star Map (EDSM) - specifically, last
    reported location and combat/merchant/exploration/CQC and naval ranks.
    
    Also adds the ability for the bot to stream FSDJump/Docked journal events from
    players submitting their data to the Elite Dangerous Data Network (EDDN), and
    when added to a per-channel tracking list in the bot.
    
    Change-Id: Iaa23c85f5f8a4a12297f0fc3f2a6fa515f6f8bcf

---
+++ src/main/java/org/lizardirc/beancounter/commands/elite/eddn/EddnMonitor.java
@@ -0,0 +1,93 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2017 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ *
+ * ============================================================================
+ *
+ * The code in this file is based on the EDDN example code for Java, which can be
+ * found at <https://github.com/AnthorNet/EDDN/tree/master/examples/Java>
+ */
+
+package org.lizardirc.beancounter.commands.elite.eddn;
+
+import java.util.zip.Inflater;
+
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class EddnMonitor implements Runnable {
+    private final EddnHandler<?> parent;
+    private volatile boolean interrupted = false;
+
+    public EddnMonitor(EddnHandler<?> parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void run() {
+        ZContext ctx = new ZContext();
+        ZMQ.Socket client = ctx.createSocket(ZMQ.SUB);
+        client.subscribe("".getBytes());
+        client.setReceiveTimeOut(30000);
+
+        client.connect(EddnHandler.ZEROMQ_ENDPOINT);
+        System.err.println("elite.eddn.EddnMonitor: EDDN ZeroMQ endpoint connected");
+        ZMQ.Poller poller = ctx.createPoller(2);
+        poller.register(client, ZMQ.Poller.POLLIN);
+        byte[] output = new byte[256 * 1024];
+        while (!interrupted) {
+            int poll = poller.poll(10);
+
+            if (poll == ZMQ.Poller.POLLIN) {
+                if (poller.pollin(0)) {
+                    byte[] recv = client.recv(ZMQ.NOBLOCK);
+                    if (recv.length > 0) {
+                        // decompress
+                        Inflater inflater = new Inflater();
+                        inflater.setInput(recv);
+                        try {
+                            int outlen = inflater.inflate(output);
+                            String outputString = new String(output, 0, outlen, "UTF-8");
+                            // outputString contains a json message
+
+                            parent.handleMessage(outputString);
+                        } catch (Exception e) {
+                            parent.signalError(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    synchronized void interrupt() {
+        interrupted = true;
+    }
+}
---
+++ src/main/java/org/lizardirc/beancounter/commands/elite/eddn/Parser.java
@@ -0,0 +1,62 @@
+/*
+ * LIZARDIRC/BEANCOUNTER
+ * By the LizardIRC Development Team (see AUTHORS.txt file)
+ *
+ * Copyright (C) 2017 by the LizardIRC Development Team. Some rights reserved.
+ *
+ * License GPLv3+: GNU General Public License version 3 or later (at your choice):
+ * <http://gnu.org/licenses/gpl.html>. This is free software: you are free to
+ * change and redistribute it at your will provided that your redistribution, with
+ * or without modifications, is also licensed under the GNU GPL. (Although not
+ * required by the license, we also ask that you attribute us!) There is NO
+ * WARRANTY FOR THIS SOFTWARE to the extent permitted by law.
+ *
+ * Note that this is an official project of the LizardIRC IRC network.  For more
+ * information about LizardIRC, please visit our website at
+ * <https://www.lizardirc.org>.
+ *
+ * This is an open source project. The source Git repositories, which you are
+ * welcome to contribute to, can be found here:
+ * <https://gerrit.fastlizard4.org/r/gitweb?p=LizardIRC%2FBeancounter.git;a=summary>
+ * <https://git.fastlizard4.org/gitblit/summary/?r=LizardIRC/Beancounter.git>
+ *
+ * Gerrit Code Review for the project:
+ * <https://gerrit.fastlizard4.org/r/#/q/project:LizardIRC/Beancounter,n,z>
+ *
+ * Alternatively, the project source code can be found on the PUBLISH-ONLY mirror
+ * on GitHub: <https://github.com/LizardNet/LizardIRC-Beancounter>
+ *
+ * Note: Pull requests and patches submitted to GitHub will be transferred by a
+ * developer to Gerrit before they are acted upon.
+ */
+
+package org.lizardirc.beancounter.commands.elite.eddn;
+
+import com.google.gson.Gson;
+
+import org.lizardirc.beancounter.commands.elite.eddn.schema.SchemaRef;
+
+public class Parser {
+    private final Gson gson;
+
+    public Parser() {
+        gson = new Gson();
+    }
+
+    public <T extends SchemaRef> T parseJson(String json, Class<T> type) throws SchemaMismatchException {
+        T retval = gson.fromJson(json, type);
+
+        if (!retval.getSchemaRef().equals(retval.getExpectedSchemaId())) {
+            throw new SchemaMismatchException("$schemaRef of the received object does not match the expected value.  " +
+                "We got: \"" + retval.getSchemaRef() + "\"; expected: \"" + retval.getExpectedSchemaId() + "\".  Is this a " +
+                type.getName() + " message?  Has the schema version changed recently?");
+        }
+
+        retval.setOriginalJson(json);
+        return retval;
+    }
+
+    public Gson getGson() {
+        return gson;
+    }
+}
